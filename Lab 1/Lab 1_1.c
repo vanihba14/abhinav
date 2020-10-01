2dfd112ea6b613fc19c82a78cc9a84d43b8cffb9
Abhinv Singh Parmar <abhi171b010@gmail.com>
2020-10-01 14:10:17 +0530

    Update Lab 1_1.c

--- Lab 1/Lab 1_1.c
+++ Lab 1/Lab 1_1.c
@@ -8,5 +8,12 @@ int main()
         printf("%d",((i/j)+1)*j);
     else
         printf("%d",((i/j)+1)*j);
+    int i,j;
+    printf("Enter value of i and j ");
+    scanf("%d%d",&i,&j);
+    if(i%j==0)
+        printf("%d",((i/j)+1)*j);
+    else
+        printf("%d",((i/j)+1)*j);
     return 0;
 }

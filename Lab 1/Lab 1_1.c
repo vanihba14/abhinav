9d89904f5e6bf2d6cbe86ccfb7ba83d46ec0b245
Yuvraj Singh Parmar <34994703+yuvraj151453@users.noreply.github.com>
2020-10-01 13:59:12 +0530

    Update Lab 1_1.c

--- Lab 1/Lab 1_1.c
+++ Lab 1/Lab 1_1.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
+    if(i%j==0)
+        printf("%d",((i/j)+1)*j);
+    if(i%j==0)
+        printf("%d",((i/j)+1)*j);
     int i,j;
     printf("Enter value of i and j ");
     scanf("%d%d",&i,&j);
